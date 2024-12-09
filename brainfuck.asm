6d2860e1528bf354366588f3102537c4e5e35838
rakRandom <fellipeleo100@gmail.com>
2024-12-08 21:04:01 -0300

    feat: add input_cell and print_debug to brainfuck

--- brainfuck.asm
+++ brainfuck.asm
@@ -1,10 +1,13 @@
 %include "include/essential.inc"
+%include "include/mathlib/module.inc"
+%include "include/stringlib/number_to_string.inc"
 %include "include/iolib/println.inc"
 %include "include/iolib/print.inc"
 %include "include/iolib/input.inc"
 
 section .data
     title_label db "Brainfuck Interpreter Shell (Assembly x86_64 Linux/Unix Version) | 'e' or '0' to exit", 10
+    out_of_bounds_error_label db "Error: Out of bounds input", 10
     input_label db "> ", 0
     max_block_size equ 32768
 
@@ -50,20 +53,21 @@ _start:
         rep stosq           ; Fill the memory with zero
 
         ; 'Variables'
-        mov r15, 0  ; Instruction pointer
-        mov r14, 0  ; Data pointer
+        mov r15, 0  ; Instruction pointer (index)
+        mov r14, 0  ; Data pointer (index)
+        mov r13, 0  ; User input buffer pointer (index)
 
         sub rsp, 8  ; Growing the stack
 
         mov qword r15, 0  
 
         .read_instruction:
-            mov rax, [code+r15]  ; Operation value
+            mov rax, [code + r15]  ; Operation value
             mov [rsp], rax
             call _operator
 
             inc r15
-            cmp byte [code+r15], NULL
+            cmp byte [code + r15], NULL
             jnz .read_instruction
         
         add rsp, 8  ; Shrinking the stack
@@ -82,32 +86,34 @@ _start:
 ; +8 - Operator value
 _operator:
     ; Jump Table
-    mov bl, byte [rsp+8]
-    cmp bl, '+'    ; Case: '+' (increase cell value)
+    mov bl, byte [rsp + 8]
+    cmp bl, '+'      ; Case: '+' (increase cell value)
     je .inc_cell
-    cmp bl, '-'    ; Case: '-' (decrease cell value)
+    cmp bl, '-'      ; Case: '-' (decrease cell value)
     je .dec_cell
-    cmp bl, '>'    ; Case: '>' (move pointer to right)
+    cmp bl, '>'      ; Case: '>' (move pointer to right)
     je .inc_ptr
-    cmp bl, '<'    ; Case: '<' (move pointer to left)
+    cmp bl, '<'      ; Case: '<' (move pointer to left)
     je .dec_ptr
-    cmp bl, '['    ; Case: '[' (start loop)
+    cmp bl, '['      ; Case: '[' (start loop)
     je .start_rep
-    cmp bl, ']'    ; Case: ']' (end loop)
+    cmp bl, ']'      ; Case: ']' (end loop)
     je .end_rep
-    cmp bl, ','    ; Case: ',' (get input)
+    cmp bl, ','      ; Case: ',' (get input)
     je .input_cell
-    cmp bl, '.'    ; Case: '.' (print cell value as ASCII)
+    cmp bl, '.'      ; Case: '.' (print cell value as ASCII)
     je .print_cell
-    
-    jmp .exit       ; Default
+    cmp bl, '#'      ; Case: '#' (print cell value as number)
+    je .print_debug
+
+    jmp .exit        ; Default
 
     .inc_cell:
-        inc byte [data+r14]
+        inc byte [data + r14]
         jmp .exit
 
     .dec_cell:
-        dec byte [data+r14]
+        dec byte [data + r14]
         jmp .exit
     
     .inc_ptr:
@@ -145,13 +151,74 @@ _operator:
         jmp .exit
     
     .input_cell:
-        ; TODO
+        ; If the user buffer is empty, ask for input
+        cmp r13, 0
+        jnz .input_cell_skip_get_input
+        input user, max_block_size
+
+        ; Getting the size of the input
+        ; NOTE: The null terminator count to the size of the input,
+        ; beucause if the user haven't written anything, the cell
+        ; value will be zero and the user buffer size should be
+        ; decreased from 1 to 0
+        xor r13, r13
+        .input_cell_loop_through_input:
+            inc r13
+            cmp byte [user + r13], 0
+            jz .input_cell_loop_done
+            cmp r13, max_block_size
+            jae .error_out_of_bounds
+            jmp .input_cell_loop_through_input
+        
+        .input_cell_loop_done:
+        
+        ; Removing the new line, also correcting the size
+        cmp r13, 0
+        jle .input_cell_skip_get_input  ; If the size is 0 or less, there was no input at all
+        mov byte [user + r13 - 1], 0
+
+        .input_cell_skip_get_input:
+
+        ; Setting the cell value
+        mov al, byte [user]      ; Setting AL (8-bits RAX) as the first value of the user buffer
+        mov byte [data+r14], al  ; Setting the current cell as the value of AL
+
+        ; Shifting the array one to the left
+        xor r8, r8  ; Counter
+        .input_cell_shift_input:
+            inc r8
+            mov al, byte [user + r8]
+            mov byte [user + r8 - 1], al  ; Setting value at R8-1 as the value at R8
+            cmp r8, r13
+            jge .input_cell_shift_done
+            jmp .input_cell_shift_input
+        
+        .input_cell_shift_done:
+        
+        dec r13  ; Decreasing the value of the user buffer size
+        
         jmp .exit
+
+        .error_out_of_bounds:
+            print out_of_bounds_error_label, 27
+            exit 1
 
     .print_cell:
         mov rcx, data
         add rcx, r14
         print rcx, 1
+
+        jmp .exit
+    
+    .print_debug:
+        mov rcx, data       ; Getting the cell address
+        add rcx, r14        ; Adding the offset
+        xor rax, rax        ; Zeroing the RAX register
+        mov al, byte [rcx]  ; Setting the least byte of the RAX (AL) as the cell value (8 bits)
+        call _number_to_string
+        mov r12, rax
+        println r12, rcx
+
         jmp .exit
     
     .exit:
@@ -209,3 +276,9 @@ _operator:
 ; < ++++++++++++++++++++++++++++++++++++++++++++++++ . > +++++++++++++++++++++++++++++++++++++++++++++++++ .
 ; Expected output: 
 ; 01
+
+; Title: Input test
+; Code: 
+; ,.
+; Expected output: 
+; (whatever character you write first)
